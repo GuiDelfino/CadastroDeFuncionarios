82cc1282b732d37fe7e138aa1b7e51b7486419f0
Guilherme Delfino <guilhermevsdelfino7@gmail.com>
2025-09-24 14:18:39 -0300

    Creating constructors, getters and setters.

--- src/main/java/dev/employee/ManagementEmployeeList/model/Employee.java
+++ src/main/java/dev/employee/ManagementEmployeeList/model/Employee.java
@@ -1,14 +1,64 @@
 package dev.employee.ManagementEmployeeList.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employee_table")
 public class Employee {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String employee;
     private String position;
     private int age;
     private String email;
+
+    public Employee(Long id, String employee, String position, int age, String email) {
+        this.id = id;
+        this.employee = employee;
+        this.position = position;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
